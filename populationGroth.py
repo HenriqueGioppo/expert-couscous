a296f64562ef464a71442ced11bb1d7db88a94d5
Henrique Gioppo Roman Ross <henriquegioppo@gmail.com>
2023-03-26 18:36:17 -0300

    Some fixes to the population growth code

--- populationGroth.py
+++ populationGroth.py
@@ -2,21 +2,18 @@
 
 def popGroth():
     #input
-    print("This program calculates the population growth in x years/n")
-    print("Enter the following information:")
+    print("This program calculates the population growth in x years/n Enter the following information:")
     pop = int(input("Enter the current population: "))
     rate = float(input("Enter the annual growth rate in %: "))
     years = int(input("Enter the number of years: "))
     #process
     rate = rate / 100
-    for i in range(years):
+    for years in range(years + 1):
         pop = pop + (pop * rate)
     #output
     #round pop to the nearest whole number
     pop = round(pop)
     print("The population in", years, "years will be", pop)
-
-    #user input
 
 
 popGroth()
